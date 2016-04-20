6a10796765fb70b4c3d693960fb9c57447c4b9ac
alexkernphysiker <kernphysiker@openmailbox.org>
2016-04-20 22:45:00 +0200

    submodule added. Class declaration

--- include/HVAdjuster.h
+++ include/HVAdjuster.h
@@ -2,12 +2,22 @@
 // MIT license
 #ifndef ______HVADJUSTER____H______
 #	define ______HVADJUSTER____H______
-
+#include <functional>
+#include <vector>
 namespace HVAdjust{
+	struct MeasuredParameters{
+		double HV;
+		double Gain;
+	};
 	class HVAdjuster{
 	public:
-		HVAdjuster();
+		HVAdjuster(const std::function<double(double)> HV2Gain, const double hv_delta);
+		HVAdjuster(const HVAdjuster&source);
 		virtual ~HVAdjuster();
+		const std::vector<double> Adjust(const std::vector<MeasuredParameters>&data)const;
+	private:
+		const std::function<double(double)> f_HV2Gain;
+		double f_hv_delta;
 	};
 };
 
--- src/HVAdjuster.cpp
+++ src/HVAdjuster.cpp
@@ -1,9 +1,17 @@
 // this file is distributed under 
 // MIT license
+#include <Genetic/equation.h>
 #include <HVAdjuster.h>
-
-HVAdjust::HVAdjuster::HVAdjuster(){
-
-}
+using namespace std;
+using namespace Genetic;
 HVAdjust::HVAdjuster::~HVAdjuster(){}
+HVAdjust::HVAdjuster::HVAdjuster(const std::function< double > HV2Gain, const double hv_delta)
+:f_HV2Gain(HV2Gain),f_hv_delta(hv_delta){}
+HVAdjust::HVAdjuster::HVAdjuster(const HVAdjust::HVAdjuster& source)
+:f_HV2Gain(source.f_HV2Gain),f_hv_delta(source.f_hv_delta){}
+const std::vector< double > HVAdjust::HVAdjuster::Adjust(const std::vector<HVAdjust::MeasuredParameters>& data) const{
+	const double HV_max=1300;
+	
+}
+
 
