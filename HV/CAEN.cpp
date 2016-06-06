4be494196094e1dbead252e4362e1fd8dfb41975
alexkernphysiker <kernphysiker@openmailbox.org>
2016-06-06 17:02:26 +0200

    cmake project update

---
+++ HV/CAEN.cpp
@@ -0,0 +1,4 @@
+// this file is distributed under 
+// MIT license
+#include <CAENHVWrapper.h>
+#include "CAEN.h"
--- HV/CAEN.h
+++ HV/CAEN.h
@@ -1,9 +1,10 @@
 // this file is distributed under 
 // MIT license
-#ifndef ____________HV___CALC_H__________
-#	define ____________HV___CALC_H__________
+#ifndef ____________HV___CAEN_H__________
+#	define ____________HV___CAEN_H__________
 #include <functional>
 #include <vector>
+#include "HVSetter.h"
 namespace HVAdjust{
 }
 #endif
