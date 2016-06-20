52e6e754516f6dbf93045a63de4d3acc1d86d41e
alexkernphysiker <kernphysiker@openmailbox.org>
2016-06-20 16:08:33 +0200

    HV refactoring. Testing HV stage 4

--- example/hv_caen.cpp
+++ example/hv_caen.cpp
@@ -24,19 +24,11 @@ int main(int argc,char**argv){
 	}
 	CAEN hv(constr);
 	cout<<"========"<<endl;
+	for(size_t idx=800;idx<832;idx++)
+		cout<<idx<<"\t"<<hv.IsOn(idx)<<"\t"<<hv.GetHV(idx)<<endl;
 	for(size_t idx=800;idx<832;idx++){
 		hv.turnOn(idx);
 		hv.SetHV(idx,500);
 	}
-	for(size_t c=4000000000;c>1;c--);
-	for(size_t idx=800;idx<832;idx++)
-		cout<<idx<<"\t"<<hv.IsOn(idx)<<"\t"<<hv.GetHV(idx)<<endl;
-	for(size_t c=4000000000;c>1;c--);
-	for(size_t idx=800;idx<832;idx++){
-		hv.turnOff(idx);
-	}
-	for(size_t c=4000000000;c>1;c--);
-	for(size_t idx=800;idx<832;idx++)
-		cout<<idx<<"\t"<<hv.IsOn(idx)<<"\t"<<hv.GetHV(idx)<<endl;
 	cout<<"========"<<endl;
 }
