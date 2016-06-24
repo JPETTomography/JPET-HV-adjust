05e1538d6b040445156920fedf1aab8e022a9aa9
alexkernphysiker <kernphysiker@openmailbox.org>
2016-06-24 16:04:07 +0200

    attempt to fix CAEN

--- hv-config-apply.cpp
+++ hv-config-apply.cpp
@@ -10,6 +10,7 @@
 #include <JPetData/HVSetter.h>
 #include <JPetData/Detectors.h>
 #include <Postgres/postgres_data.h>
+#include <libhvexception.h>
 #include <HV/CAEN.h>
 #include "config-read.h"
 using namespace std;
@@ -38,36 +39,42 @@ int main(int argc,char**argv){
 						});
 						if(hvconfigsearch.size()>0){
 							if(hvconfigsearch.size()==1){
-								HighVoltage highvoltage=HighVoltageTable(db).ByID(setupsearch[0].highvoltage_id());
-								HVTable hvtable(hvconfigsearch[0],setupsearch[0],framesearch[0],highvoltage,db,hv);
-								cout<<"Writting HV"<<endl;
-								hvtable.SynchroHardwarewithDB();
-								for(bool matches=false;!matches;){
-									cout<<"waiting..."<<endl;
-									this_thread::sleep_for(chrono::seconds(10));
-									cout<<"Reading HV..."<<endl;
-									hvtable.read_hardware();
-									matches=true;
-									for(size_t index=0;index<hvtable.SlotInfo().size();index++){
-										cout<<"IDX="<<hvtable.SlotInfo()[index].hvchannel.idx()<<"...";
-										if(isfinite(hvtable.HVConfigEntries()[index].HV())){
-											cout<<"comparing...";
-											if(isfinite(hvtable.HardwareHV()[index])){
-												bool res=(pow(hvtable.HVConfigEntries()[index].HV()-hvtable.HardwareHV()[index],2)<pow(max_difference,2));
-												if(res)cout<<"OK.";
-												else cout<<"not OK.";
-												matches&=res;
-											}else{
-												matches=false;
-												cout <<"HV hardware unknown error (infinite value was read). Code=2"<<endl;
-												return 2;
+								try{
+									HighVoltage highvoltage=HighVoltageTable(db).ByID(setupsearch[0].highvoltage_id());
+									HVTable hvtable(hvconfigsearch[0],setupsearch[0],framesearch[0],highvoltage,db,hv);
+									cout<<"Writting HV"<<endl;
+									hvtable.SynchroHardwarewithDB();
+									for(bool matches=false;!matches;){
+										cout<<"waiting..."<<endl;
+										this_thread::sleep_for(chrono::seconds(10));
+										cout<<"Reading HV..."<<endl;
+										hvtable.read_hardware();
+										matches=true;
+										for(size_t index=0;index<hvtable.SlotInfo().size();index++){
+											cout<<"IDX="<<hvtable.SlotInfo()[index].hvchannel.idx()<<"...";
+											if(isfinite(hvtable.HVConfigEntries()[index].HV())){
+												cout<<"comparing...";
+												if(isfinite(hvtable.HardwareHV()[index])){
+													bool res=(pow(hvtable.HVConfigEntries()[index].HV()-hvtable.HardwareHV()[index],2)<pow(max_difference,2));
+													if(res)cout<<"OK.";
+													else cout<<"not OK.";
+													matches&=res;
+												}else{
+													matches=false;
+													cout <<"HV hardware unknown error (infinite value was read). Code=2"<<endl;
+													return 2;
+												}
+												cout<<endl;
 											}
-											cout<<endl;
+											cout<<"not configured"<<endl;
 										}
-										cout<<"not configured"<<endl;
+										cout<<"Success"<<endl;
+										return 0;
 									}
-									cout<<"Success"<<endl;
-									return 0;
+								}catch(libhvException* ex){
+									cout<<"HVERROR!!!!!: "<< ex->getMessage()<<endl;
+									cout<<"Code = 3"<<endl;
+									return 3;
 								}
 							}else cout<<"hvconfig name is not inique"<<endl;
 						}else cout<<"hvconfig not found"<<endl;
