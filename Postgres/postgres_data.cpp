7f932e1ced130e76902ca641b154ba8863c1689d
alexkernphysiker <kernphysiker@openmailbox.org>
2016-06-29 15:32:15 +0200

    HVConfig fix

--- Postgres/postgres_data.cpp
+++ Postgres/postgres_data.cpp
@@ -65,7 +65,7 @@ namespace DataAccess{
 			case data_hvconfig:
 				switch(request.operation){
 				case data_obtain: sql_request="select * from \"HVConfig\" where setup_id="+P[0]+";";break;
-				case data_insert: sql_request="insert into \"HVConfig\" (setup_id,description)values("+P[0]+","+P[1]+");"; break;
+				case data_insert: sql_request="insert into \"HVConfig\" (setup_id,description)values("+P[1]+","+P[0]+");"; break;
 				case data_remove: sql_request="delete from \"HVConfig\" where id="+P[0]+";"; break;
 				};
 			break;
